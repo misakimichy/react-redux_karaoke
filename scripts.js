ea2cb39260718914462fd95fe20e773abf3e4986
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-23 19:28:33 -0800

    Combine reducers with test.

--- scripts.js
+++ scripts.js
@@ -58,6 +58,15 @@ const songChangeReducer = (state = initialState.currentSongId, action) => {
       return state
   }
 }
+
+const rootReducer = this.Redux.combineReducers({
+  currentSongId: songChangeReducer,
+  songsById: lyricChangeReducer
+})
+
+// Redux store
+const { createStore } = Redux
+const store = createStore(rootReducer)
     
 // Jest tests + setup
 const { expect } = window
@@ -101,32 +110,82 @@ expect(songChangeReducer(initialState, { type: null })).toEqual(initialState)
 
 expect(songChangeReducer(initialState.currentSongId, { type: 'CHANGE_SONG', newSelectedSongId: 1 })).toEqual(1)
 
-// Redux store
-const { createStore } = Redux
-const store = createStore(lyricChangeReducer)
+expect(rootReducer(initialState, { type: null })).toEqual(initialState)
 
-// // Rendering State in DOM
-// const renderLyrics = () => {
-//   const lyricsDisplay = document.getElementById('lyrics')
-//   while(lyricsDisplay.firstChild) {
-//     lyricsDisplay.removeChild(lyricsDisplay.firstChild)
-//   }
-//   const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition]
-//   const renderedLine = document.createTextNode(currentLine)
-//   document.getElementById('lyrics').appendChild(renderedLine)
-// }
+expect(store.getState().currentSongId).toEqual(songChangeReducer(undefined, { type: null }))
 
-// window.onload = () => {
-//   renderLyrics()
-// }
+expect(store.getState().songsById).toEqual(lyricChangeReducer(undefined, { type: null }))
 
-// // Click Listener
-// const userClick = () => {
-//   const currentState = store.getState()
-//   currentState.arrayPosition === currentState.songLyricsArray.length - 1
-//   ? store.dispatch({ type: 'RESTART_SONG'})
-//   : store.dispatch({ type: 'NEXT_LYRIC' })
-// }
+// Rendering State in DOM
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics')
+  while(lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild)
+  }
+  if(store.getState().currentSongId) {
+    const currentLine = document.createTextNode(store.getState().songsById[store.getState().currentSongId].songArray[store.getState().songsById[store.getState().currentSongId].arrayPosition])
+    document.getElementById('lyrics').appendChild(currentLine)
+  } else {
+    const selectSongMessage = document.createTextNode("Select a song from the menu above to sing along!")
+    document.getElementById('lyrics').appendChild(selectSongMessage)
+  }
+}
 
-// // Subscribe to Redux store
-// store.subscribe(renderLyrics)
+const renderSongs = () => {
+  const songsById = store.getState().songsById
+  for(let songKey in songsById) {
+    const song = songsById[songKey]
+    const li = document.createElement('li')
+    const h3 = document.createElement('h3')
+    const em = document.createElement('em')
+    const songTitle = document.createTextNode(song.title)
+    const songArtist = document.createTextNode(`by ${song.artist}`)
+    em.appendChild(songTitle)
+    h3.appendChild(em)
+    h3.appendChild(songArtist)
+    h3.addEventListener('click', () => {
+      selectSong(song.songId)
+    })
+    li.appendChild(h3)
+    document.getElementById('songs').appendChild(li)
+  }
+}
+
+window.onload = () => {
+  renderSongs()
+  renderLyrics()
+}
+
+// Click Listener
+const userClick = () => {
+  if (store.getState().songsById[store.getState().currentSongId].arrayPosition === store.getState().songsById[store.getState().currentSongId].songArray.length - 1) {
+    store.dispatch({
+      type: 'RESTART_SONG',
+      currentSongId: store.getState().currentSongId
+    })
+  } else {
+    store.dispatch({
+      type: 'NEXT_LYRIC',
+      currentSongId: store.getState().currentSongId
+    })
+  }
+}
+
+const selectSong = newSongId => {
+  let action
+  if(store.getState().currentSongId) {
+    action = {
+      type: 'RESTART_SONG',
+      currentSongId: store.getState().currentSongId
+    }
+    store.dispatch(action)
+  }
+  action = {
+    type: 'CHANGE_SONG',
+    newSelectedSongId: newSongId
+  }
+  store.dispatch(action)
+}
+
+// Subscribe to Redux store
+store.subscribe(renderLyrics)
