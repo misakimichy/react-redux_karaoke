1567b61f30fdd0544e3115b0175b1633841665bc
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-23 18:26:01 -0800

    Add Redix reducer and jest test.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,39 @@
+const songLyricsArray = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye".split(', ');
+
+const initialState = {
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 0
+}
+
+// Redux Reducer
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case 'NEXT_LYRIC':
+      let newArrayPosition = state.arrayPosition + 1;
+      let newState = {
+        songLyricsArray: state.songLyricsArray,
+        arrayPosition: newArrayPosition,
+      }
+      return newState
+    default:
+      return state
+  }
+}
+    
+// Jest tests + setup
+const { expect } = window
+expect(reducer(initialState, { type: null })).toEqual(initialState)
+
+expect(reducer(initialState, { type: 'NEXT_LYRIC' })).toEqual({
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 1
+})
+
+// Redux store
+const { createStore } = Redux
+const store = createStore(reducer)
+console.log(store.getState())
+
 const userClick = () => {
   console.log('click');
 }
